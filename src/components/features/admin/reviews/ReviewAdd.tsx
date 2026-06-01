7d99f1ba93f71e6394457eb349e79db0eac15022
fazilyousuf <fazifazu01@gmail.com>
2026-06-01 11:09:45 +0530

    Resolved Build errors

--- src/components/features/admin/reviews/ReviewAdd.tsx
+++ src/components/features/admin/reviews/ReviewAdd.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { type Review } from '@/types/review';
-import { getReviews, saveReviews } from '@/services/reviewsService';
+import { saveReviews } from '@/services/reviewsService';
 import '../Admin.css';
 
 export default function ReviewAdd() {
